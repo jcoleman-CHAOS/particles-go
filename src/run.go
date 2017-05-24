9e10bacdff447c60e501496f4347d416383c5458
James Coleman <jrc2494@gmail.com>
2017-05-24 08:33:24 -0400

    json parsed in go routine

--- src/run.go
+++ src/run.go
@@ -5,14 +5,30 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"reflect"
-	s "strings"
+	"strings"
 
 	"github.com/r3labs/sse"
 )
 
 // Probably not best practice but is conventient
 // var println = fmt.Println
+
+type JSONableSlice []uint8
+
+func (u JSONableSlice) MarshalJSON() ([]byte, error) {
+	var result string
+	if u == nil {
+		result = "null"
+	} else {
+		result = strings.Join(strings.Fields(fmt.Sprintf("%d", u)), ",")
+	}
+	return []byte(result), nil
+}
+
+type Test struct {
+	Name  string
+	Array JSONableSlice
+}
 
 func check(e error) {
 	if e != nil {
@@ -40,27 +56,57 @@ func parseCreds(lines []string) map[string]string {
 	// results := make([]string, 2)
 	for _, line := range lines {
 		// var res []string
-		if s.HasPrefix(line, "#") {
+		if strings.HasPrefix(line, "#") {
 			// ignore it
 		} else {
-			res := s.Split(line, "=")
+			res := strings.Split(line, "=")
 			settings[res[0]] = res[1]
 		}
 	}
 	return settings
 }
 
-//EventsReponse is the response of the Particle events API
+//EventsReponse is the response of the Particle events API.
 type EventsResponse struct {
 	Data        string `json:"data"`
 	PublishedAt string `json:"published_at"`
 	CoreID      string `json:"coreid"`
 }
 
-//InfluxWriteString
+//InfluxWriteString values.
 type InfluxWriteString struct {
 	// Must conform to:
 	// weather,location=us-midwest,season=summer temperature=82 1465839830100400200
+}
+
+//GenericSensor struct.
+type GenericSensor struct {
+	label string
+	phen  string //Phenomanon
+	unit  string
+
+	location   string  // Qualitatively
+	lat        float64 // as float
+	long       float64
+	Experiment string
+
+	Firmware    string
+	PublishRate int64 //milliseconds
+}
+
+// parses Particle API json
+func EventsAPIJSON(u []uint8) map[string]interface{} {
+	s := string(u)
+	var formattedJSON map[string]interface{}
+	err := json.Unmarshal([]byte(s), &formattedJSON)
+	if err != nil {
+		if err.Error() == "unexpected end of JSON input" {
+			// pass
+		} else {
+			panic(err)
+		}
+	}
+	return formattedJSON
 }
 
 func main() {
@@ -84,13 +130,8 @@ func main() {
 
 	client := sse.NewClient(sseURL)
 	go client.Subscribe("messages", func(msg *sse.Event) {
-		str := string(msg.Data)
-		res := EventsResponse{}
-		json.Unmarshal([]byte(str), &res)
 		fmt.Println(string(msg.Event))
-		fmt.Println(reflect.TypeOf(string(msg.Event)))
-		// fmt.Println(reflect.TypeOf(msg.Event))
-		// fmt.Println(res)
+		fmt.Println(EventsAPIJSON(msg.Data))
 	})
 
 	var input string
